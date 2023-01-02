ffbf01fa1f5c95e3b8fba748d35860eece6aee22
Pedro Costa <pedrohsc100@gmail.com>
2023-01-02 08:58:02 -0300

    retirado codigos comentados da entrega m4 s5

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -11,11 +11,6 @@ categoryRouter.get("/categories",  getCategoriesController );
 
 categoryRouter.get("/categories/:id",  getOneCategoryController);
 
-// categoryRouter.post("/login", LoginUserMiddleware, loginUsersController);
-
-// categoryRouter.patch("/users/:id", validateUserMiddleware,  updateUserMiddleware(patchUserShape),  patchUserController);
-
-// categoryRouter.delete("/users/:id",  validateUserMiddleware,  isAdmValidationMiddleware, deleteUserController);
 
 
 export default categoryRouter
